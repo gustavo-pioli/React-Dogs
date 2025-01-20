da076a278817fa2c6cc26dece89e32857190a558
gustavo-pioli <gustavo_pioli@hotmail.com>
2025-01-20 09:59:20 -0300

    Integração API

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+const Login = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
+  return (
+    <div>
+      <Routes>
+        <Route path="/*" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Login;
